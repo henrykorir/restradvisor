43ea4318d74b5e6f3869b390ea0c426b0efb64a8
henrykorir <longkhery@gmail.com>
2021-01-19 01:34:41 +0300

    Added star clicking to symbolize a rating, added text area for comment

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,26 +1,41 @@
 import React from 'react';
-//import StarGroup from '../components/StarGroup';
+import StarGroup from '../components/StarGroup';
 
 class ReviewForm extends React.Component{
 	constructor(props){
 		super(props);
+		this.state={
+			rating: 0,
+		}
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleRating = this.handleRating.bind(this);
+	}
+	handleRating(star){
+		this.setState((state) => {
+			return {rating: star};
+		});
 	}
 	handleSubmit(e){
-		this.props.onClick(1);
-		console.clear();
-		console.log(e);
+		const stars = this.state.rating <= 5 ? this.state.rating : 5;
+		const comment = e.target[1].value;
+		e.target[1].value = "";
+		this.props.onPostReview({stars:stars, comment:comment});
+		document.querySelectorAll(".review i").forEach((element) =>element.classList.remove("checked"));
+		this.setState({rating: 0});
 		e.preventDefault();
 	}
+	
 	render(){
 		return(
-			<div className="review">
-				<form onSubmit={this.handleSubmit} >
+			<form className="review" onSubmit={this.handleSubmit} >
+				<fieldset>
+					<legend style={{"textAlign":"center"}}>Add Review</legend>
+					<StarGroup stars = {0} onRating= {this.handleRating} />
 					<textarea id="comment" name="comment">
 					</textarea>
-					<input type="submit" id="submit" value="Submit Your Review"/>
-				</form>
-			</div>
+					<input style={{"float": "right"}}type="submit" id="submit" value="Submit"/>
+				</fieldset>
+			</form>
 		);
 	}
 }
