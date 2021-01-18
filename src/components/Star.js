3e427e2165320e4a20a99b190580d84f7026dbea
henrykorir <longkhery@gmail.com>
2021-01-19 01:36:24 +0300

    Added hover detection by previous siblings and updating of the rating status

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,9 +1,30 @@
 import React from 'react';
 class Star extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			on: false,
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e){
+		this.setState({on: true});
+		e.target.classList.add("checked");
+		let count = 1;
+		let nextSibling = e.target.nextElementSibling;
+		while(nextSibling) {
+			nextSibling.classList.add("checked");
+			nextSibling = nextSibling.nextElementSibling;
+			count++;
+		}
+		this.props.onRating(count);
+	}
 	render(){
-		const style = "fa fa-star " + this.props.status;
+		const clicked = this.state.on === true ? "checked" : "";
+		const status = this.props.status === undefined ? "" : this.props.status;
+		const style = "fa fa-star " + status + clicked;
 		return(
-			<i className= {style} ></i>
+			<i className= {style} onClick={this.handleClick}></i>
 		);
 	}
 }
