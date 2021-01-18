a716aee77b6f58c2b4616a9e7b72fc509d199f78
henrykorir <longkhery@gmail.com>
2021-01-19 01:28:46 +0300

    Modified and added handlers to react to new reviews

--- src/components/DetailsTab.js
+++ src/components/DetailsTab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RestaurantProfile from '../components/RestaurantProfile';
 import PostedReview from '../components/PostedReview';
-import ReviewForm from '../components/ReviewForm';
+
 class DetailsTab extends React.Component{
 	constructor(props){
 		super(props);
@@ -10,6 +10,7 @@ class DetailsTab extends React.Component{
 			record: props.data[0],
 		}
 		this.handleClick = this.handleClick.bind(this);
+		this.handleReview = this.handleReview.bind(this);
 	}
 	handleClick(info){
 		this.setState(( state ) =>{
@@ -17,22 +18,25 @@ class DetailsTab extends React.Component{
 		});
 		this.props.onShowHere({lat: info.record.lat, lng: info.record.long});
 	}
+	handleReview(review){
+		console.log(review);
+		let record = this.state.record;
+		record.ratings.unshift(review);
+		this.setState({record: record});
+	}
 	render(){
 		let tab;
 		switch(this.state.status){
 			case 1:
 				tab = this.props.data.map((info,i) =>
 						<li key={i.toString()}>
-							<RestaurantProfile record = { info } onClick={ this.handleClick }/>
+							<RestaurantProfile record = { info } fixed={false}onClick={ this.handleClick }/>
 						</li>
 					)
 				tab = <ul>{tab}</ul>;
 				break;
 			case 2:
-				tab = <PostedReview record={ this.state.record } onClick={ this.handleClick }/>;
-				break;
-			case 3:
-				tab = <ReviewForm onClick ={ this.handleClick }/>;
+				tab = <PostedReview record={ this.state.record } onClick={ this.handleClick } onPostReview = {this.handleReview}/>;
 				break;
 			default:
 				break;
