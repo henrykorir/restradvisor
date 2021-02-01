81584ee7ca24a2599634b7eebcb104dd3add5986
henrykorir <longkhery@gmail.com>
2021-02-02 01:32:11 +0300

    Refactor to use useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useReducer} from 'react';
 import Header from './components/Header';
 import Map from './components/Map';
 import DetailsTab from './components/DetailsTab';
@@ -6,41 +6,59 @@ import information from './database/data.json';
 import getAverageRating from './helper/getAverageRating';
 import './App.css';
 
+const initialState = {
+	originalData: information,
+	data: information
+}
+const reducer = (state, action) =>{
+	switch(action.type){
+		case 'ADD_NEW_PLACE':
+			return {
+				...state, originalData:[...state.originalData, action.payload], data: [...state.data, action.payload]
+			}
+		case 'FILTER_BY_RATINGS_RANGE':
+			return{
+				...state, originalData: state.originalData, data: action.payload
+			}
+		case 'GET_GOOGLE_PLACES':
+			const allData = [...state.originalData, action.payload];
+			return {
+				...state, originalData: allData, data: allData
+			}
+		case 'GET_AVERAGE_RATING':
+			return {
+				...state, originalData: action.payload, data: action.payload
+			}
+		default:
+			return state;
+	}
+}
 function App() {
 	const [here, setHere] = useState({lng: 151.215, lat: -33.856});
-	const [data, setData] = useState(information);
-	const [min, setMin] = useState(1);
-    const [max, setMax] = useState(5);
-
+	const [state, dispatch] = useReducer(reducer,initialState);
+	
 	const changeLocation = (coords) => {
         setHere(coords);
     };
 	const handleFilter =(a, b) =>{
 		const min = Math.min(a,b);
 		const max = Math.max(a,b);
-		setMin(min);
-		setMax(max);
+		let filteredData = state.originalData;
+		filteredData = filteredData.filter((place) =>(place.averageRating >= min) && (place.averageRating <= max));
+		dispatch({type:'FILTER_BY_RATINGS_RANGE', payload: filteredData});
 	};
 	const addANewPlace = (newPlace) =>{
-		let temp = [...data, newPlace];
-		setData(temp);
+		dispatch({type: 'ADD_NEW_PLACE', payload: newPlace});
 	}
 	
 	useEffect(() =>{
-		data.forEach((place,i) =>{
-			place.averageRating = getAverageRating(place.ratings);
+		let temp = state.originalData;
+		temp.forEach((place) => {
+			place.averageRating = getAverageRating(place.ratings)
 		});
-		console.log(data);
-		setData(data);
-	},[data]);
-	useEffect(() => {
-        let filteredData = data;
-		filteredData = filteredData.filter((place) =>((place.averageRating >= min) && (place.averageRating <= max)));
-		setData(filteredData);
-    }, [min, max, data]);
-	
+		dispatch({type:'GET_AVERAGE_RATING', payload: temp});
+	},[state.originalData]);
 	useEffect(() =>{
-		console.log(data);
 		navigator.geolocation.getCurrentPosition(
 			(position) => {
 				setHere({lng: position.coords.longitude, lat: position.coords.latitude});
@@ -48,29 +66,27 @@ function App() {
 				fetch(nearby_url)
 				.then(nearbyResponse => nearbyResponse.json())
 				.then(nearbyPlaces =>{
-							
 					nearbyPlaces.results.forEach((place, i) =>{
 						let place_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id="+place.place_id+"&fields=name,formatted_address,geometry,rating,reviews&key=AIzaSyA8CgnGnHEkyeweyqk-Abf-BjhRb_j2o90";
 						return fetch(place_url)
 						.then(response => response.json())
-						.then(data => {
+						.then(resultData => {
 							let reviews = [];
-							if(data.result.reviews !== undefined){
-								for(let review of data.result.reviews){
+							if(resultData.result.reviews !== undefined){
+								for(let review of resultData.result.reviews){
 									reviews.push({stars: review.rating, comment: review.text});
 								}
 							}
-							let rate = data.result.rating === undefined ? 1 : Math.trunc(data.result.rating); 
-							information.push({
-								restaurantName: data.result.name,
-								address: data.result.formatted_address,
-								lat: data.result.geometry.location.lat,
-								long: data.result.geometry.location.lng,
+							let rate = resultData.result.rating === undefined ? 1 : Math.trunc(resultData.result.rating); 
+							let googlePlace ={
+								restaurantName: resultData.result.name,
+								address: resultData.result.formatted_address,
+								lat: resultData.result.geometry.location.lat,
+								long: resultData.result.geometry.location.lng,
 								ratings: reviews,
 								averageRating: rate
-							});
-							console.log(information);
-							
+							}
+							dispatch({type: 'GET_GOOGLE_PLACES', payload: googlePlace});
 						})
 						.catch(err => console.log(err));
 					});
@@ -82,13 +98,14 @@ function App() {
 				alert("Error retriving location");
 			}
 		);
-		setData(information);
-	},[data]);
+		
+	},[]);
+
 	return (
 		<>
 			<Header onFilter={handleFilter} />
-			<Map  data = {data}  here = {here} addPlace = {addANewPlace}/>
-			<DetailsTab  onShowHere={changeLocation} data = {data} />
+			<Map  data = {state.data}  here = {here} addPlace = {addANewPlace}/>
+			<DetailsTab  onShowHere={changeLocation} data = {state.data} />
 		</>
 	);
 }
