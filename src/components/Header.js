7f390520431d6c66ae4baad9dda5525bda248711
henrykorir <longkhery@gmail.com>
2021-02-06 01:48:44 +0300

    made the slider more visible

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,7 @@ class Header extends React.Component{
 	render(){
 		return(
 			<header className="heading">
-				
 				<RatingsFilter onFilter={this.props.onFilter}/>
-				<h1>Restradvisor</h1>
 			</header>
 		);
 	}
