fa72768ebe0c8286f7234a32c8905914ae368cf2
henrykorir <longkhery@gmail.com>
2021-02-07 22:59:20 +0300

    Refactored heading and filter tool

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ class Header extends React.Component{
 	render(){
 		return(
 			<header className="heading">
+				<h1>Restradvisor</h1>
 				<RatingsFilter onFilter={this.props.onFilter}/>
 			</header>
 		);
